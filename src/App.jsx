bd76c4ec30cb8894a5c8415a615565869b6fc2c3
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-20 10:19:54 -0300

    todo1 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,28 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// (done) todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
 // todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
 // todo - Desabilite o botão de Login equanto você está executando o login.
 // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = event => {
+        const value = event.target.value;
+        console.log(value);
+        setEmail(value);
+    };
+
+    const handlePassword = event => {
+        const value = event.target.value;
+        console.log(value);
+        setPassword(value);
+    };
+
     return (
         <div className="wrapper">
             <div className="login-form">
@@ -23,11 +38,11 @@ export default function LoginForm() {
                 <div className="errorMessage"></div>
                 <div className="row">
                     <label htmlFor={'email'}>Email</label>
-                    <input id={'email'} type={'email'} autoComplete="off" />
+                    <input id={'email'} type={'email'} autoComplete="off" value={email} onChange={handleEmail} />
                 </div>
                 <div className="row">
                     <label htmlFor={'password'}>Password</label>
-                    <input id={'password'} type={'password'} />
+                    <input id={'password'} type={'password'} value={password} onChange={handlePassword} />
                 </div>
 
                 <div className="button">
