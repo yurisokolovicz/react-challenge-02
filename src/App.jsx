3b5a0b31b68dcd76e438131f1bf2a845dd472f76
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-20 09:33:14 -0300

    adjusting

--- src/App.jsx
+++ src/App.jsx
@@ -15,25 +15,25 @@ import { useState } from 'react';
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
-  return (
-    <div className='wrapper'>
-      <div className='login-form'>
-        <h1>Login Form 🐞</h1>
-        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
-        <div className='row'>
-          <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
-        </div>
-        <div className='row'>
-          <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
-        </div>
+    return (
+        <div className="wrapper">
+            <div className="login-form">
+                <h1>Login Form 🐞</h1>
+                {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+                <div className="errorMessage"></div>
+                <div className="row">
+                    <label htmlFor={'email'}>Email</label>
+                    <input id={'email'} type={'email'} autoComplete="off" />
+                </div>
+                <div className="row">
+                    <label htmlFor={'password'}>Password</label>
+                    <input id={'password'} type={'password'} />
+                </div>
 
-        <div className='button'>
-          <button>Login</button>
+                <div className="button">
+                    <button>Login</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
